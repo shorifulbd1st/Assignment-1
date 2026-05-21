afbf313a8ba28ff8ad83c5f3822d029d61001f5a
unknown <shoriful.dev95@gmail.com>
2026-05-21 08:20:01 +0600

    problem-3

--- solutions.ts
+++ solutions.ts
@@ -5,3 +5,13 @@ const filterEvenNumbers = (numbers: number[]): number[] => {
 const reverseString = (input: string): string => {
   return input.split("").reverse().join("");
 };
+
+type StringOrNumber = string | number;
+
+const checkType = (input: StringOrNumber): string => {
+  if (typeof input === "string") {
+    return "String";
+  } else {
+    return "Number";
+  }
+};
